3155e441b8bb9613db77cd023cf1834a768858c0
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2021-04-27 16:45:26 -0300

    update challenge 6

---
+++ Semana 2 - GoStack/01 Iniciando back-end do app/Desafio 6/README.md
@@ -0,0 +1,6 @@
+<h1>Terceiro desafio Node.js🚀</h1>
+
+🚀Nesse desafio foram testados todos os conceitos ensinados na aula, como `database`, `middlewares na requisição`, `repositories`, `services`, `models`, `routes`, dentre outros.😍
+
+Em contexto geral, o desafio nos fez criar um sistema de banco, assim como no desafio 5. Nele, criamos uma rota chamada `/transactions` que é para as nossas transações. Nela criamos uma rota `DELETE` para deletar uma transação, uma rota `GET` para listar nossas transações e duas rotas `POST`. A primeira é para criar a transação e a segunda é para consumir um arquivo `CSV` com transações nele. ❤🚀
+
---
+++ Semana 2 - GoStack/01 Iniciando back-end do app/Desafio 6/src/services/ImportTransactionsService.ts
@@ -0,0 +1,94 @@
+/* eslint-disable array-callback-return */
+import { getCustomRepository, getRepository, In } from 'typeorm';
+
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+// eslint-disable-next-line prettier/prettier
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+class ImportTransactionsService {
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+    const contactsReadStream = fs.createReadStream(filePath);
+
+    const parsers = csvParse({
+      from_line: 2,
+    });
+
+    const parseCSV = contactsReadStream.pipe(parsers);
+
+    const transactions: CSVTransaction[] = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', async line => {
+      // eslint-disable-next-line prettier/prettier
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+
+      transactions.push({
+        title,
+        type,
+        value,
+        category,
+      });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    const existentCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+    const existentCategoriesTitles = existentCategories.map(
+      // eslint-disable-next-line prettier/prettier
+      (category: Category) => category.title);
+
+    // eslint-disable-next-line prettier/prettier
+    const addCategoryTitles = categories.filter(
+      category => !existentCategoriesTitles.includes(
+        category)).filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoriesRepository.create(
+      addCategoryTitles.map(title => ({
+        title,
+      })),
+    );
+
+    await categoriesRepository.save(newCategories);
+
+    const finalCategories = [...newCategories, ...existentCategories];
+
+    const createdTransactions = transactionRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+    await transactionRepository.save(createdTransactions);
+
+    await fs.promises.unlink(filePath);
+
+    return createdTransactions;
+  }
+}
+
+export default ImportTransactionsService;
