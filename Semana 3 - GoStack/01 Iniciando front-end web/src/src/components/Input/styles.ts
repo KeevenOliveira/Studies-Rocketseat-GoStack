614472f0f1b730ff5b6897a493d5111191504f7f
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2021-05-19 11:28:01 -0300

    unform, cadaster validation, usability of input

--- Semana 3 - GoStack/01 Iniciando front-end web/src/src/components/Input/styles.ts
+++ Semana 3 - GoStack/01 Iniciando front-end web/src/src/components/Input/styles.ts
@@ -1,19 +1,36 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+    isFocused: boolean;
+    isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
     background: #232129;
     border-radius: 10px;
-    border: 2px solid #232129;
     padding: 16px;
     width: 100%;
     
     display: flex;
     align-items: center;
+    
     color: #666360;
+    border: 2px solid #232129;
 
     & + div{
         margin-top: 8px;
     }
+
+    ${props => props.isFocused && css`
+        color: #FF9000;
+        border-color: #FF9000;
+    `}
+
+    ${props => props.isFilled && css`
+        color: #FF9000;
+        /* border-color: #FF9000; //fica muito massa essa ideia do background, mas não irei deixar */
+    `}
+
     input{
 
         background: transparent;
--- Semana 3 - GoStack/01 Iniciando front-end web/src/src/pages/SignUp/index.tsx
+++ Semana 3 - GoStack/01 Iniciando front-end web/src/src/pages/SignUp/index.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import * as Yup from 'yup';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import { Container, Content, Background } from './styles'
 
-const SignUp: React.FC = () => (
-    <Container>
+
+const SignUp: React.FC = () => {
+
+    const handleSubmit = useCallback(async (data: object) => {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string().required('Email obrigatório').email('Digite um email válido'),
+                password: Yup.string().min(6, 'No mínimo 6 caracteres')
+            });
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }, [])
+
+    return (
+        <Container>
         <Background/>
         <Content>
             <img src={logoImg} alt="GoBarber"/>
-            <form>
+            <Form onSubmit={handleSubmit} >
                 <h1>Faça seu Cadastro</h1>
 
                 <Input name="name" icon={FiUser} placeholder="Nome"/>
@@ -23,7 +44,7 @@ const SignUp: React.FC = () => (
 
                 <Button type="submit">Cadastrar</Button>
 
-            </form>
+            </Form>
 
             <a href="forgot">
                 <FiArrowLeft/>
@@ -31,6 +52,7 @@ const SignUp: React.FC = () => (
             </a>
         </Content>
     </Container>
-);
+    );
+}
 
 export default SignUp;
