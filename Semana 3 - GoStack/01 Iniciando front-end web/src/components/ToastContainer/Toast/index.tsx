e11da7f2b9c1e18665ddb98e0f6b061b724e759d
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2021-07-18 10:20:25 -0300

    animations toasts

--- Semana 3 - GoStack/01 Iniciando front-end web/src/components/ToastContainer/Toast/index.tsx
+++ Semana 3 - GoStack/01 Iniciando front-end web/src/components/ToastContainer/Toast/index.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi'
+import { FiAlertCircle, FiXCircle, FiInfo, FiCheckCircle } from 'react-icons/fi'
 import { ToastMessage, useToast } from '../../../hooks/ToastContext';
-
 import { Container } from './styles';
 
 interface ToastProps { 
     message: ToastMessage;
+    style: object;
 }
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const icons = {
+    info: <FiInfo size={24}/>,
+    error: <FiAlertCircle size={24}/>,
+    success: <FiCheckCircle size={24}/>
+}
+
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
 
     const { removeToast } = useToast();
 
@@ -16,11 +22,15 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
         const timer = setTimeout(()=>{
             removeToast(message.id);
         }, 3000)
+
+        return() => {
+            clearTimeout(timer);
+        }
     },[message.id, removeToast])
 
     return(
-        <Container type={message.type} hasDescription={!!message.description}>
-            <FiAlertCircle size={20}/>
+        <Container type={message.type} hasDescription={!!message.description} style={style}>
+            {icons[message.type|| 'info']}
             <div>
                 <strong>{message.title}</strong>
                     {message.description && <p>{message.description}</p>}
